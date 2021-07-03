9fc8e13936e812992471bf03edfe3c2d525c63e1
yuki---7777 <yukixxx7777@gmail.com>
2021-07-03 17:37:32 +0900

    カウンターの発展１

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,10 +1,21 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const doublebutton = document.getElementById("double-button")
+const minusbutton = document.getElementById("minus-button")
 let count = 0
 //ボタンをクリックした時の処理を登録
 plusButton.onclick = function() {
   //countを更新
   count += 1
   //count を表示
+  display.textContent = count
+}
+
+doublebutton.onclick = function() {
+  count *= 2
+  display.textContent = count
+}
+minusbutton.onclick = function() {
+  count -= 1
   display.textContent = count
 }
