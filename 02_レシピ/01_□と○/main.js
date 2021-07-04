0fe47e9a7eba231200da7f527e28ce85597e3fdb
yuki---7777 <yukixxx7777@gmail.com>
2021-07-04 15:54:08 +0900

    ストップウォッチをやった

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,14 @@
 const figure = document.getElementById("figure")
 
 figure.onclick = function() {
-  figure.classList.toggle("rounded")
+  if (figure instanceof square) {
+    figure.classList.remove("square")
+    figure.classList.add("circle")
+  } else if (figure instanceof "circle") {
+    figure.classList.remove("circle")
+    figure.classList.add("triangle")
+  } else {
+    figure.classList.remove("triangle")
+    figure.classList.add("square")
+  }
 }
---
+++ 02_レシピ/01_ストップウォッチ/main.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display")
+const button = document.getElementById("button")
+
+let count = 0
+
+const countUp = function() {
+  count += 0.01
+  display.textContent = count
+}
+let id = null
+button.onclick = function() {
+  if (id === null) {
+    id = setInterval(countUp, 10)
+    button.textContent = "stop"
+  } else {
+    clearInterval(id)
+    id = null
+    button.textContent = "start"
+  }
+}
+//発展２　ボタンを押してからromを通って実行されるまでのラグが生じるから
