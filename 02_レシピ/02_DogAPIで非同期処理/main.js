24a7b91dc785c2a28879ba70c392ce76333fe46e
yuki---7777 <yukixxx7777@gmail.com>
2021-07-18 18:48:48 +0900

    自販機

--- 02_レシピ/02_DogAPIで非同期処理/main.js
+++ 02_レシピ/02_DogAPIで非同期処理/main.js
@@ -1,7 +1,7 @@
 const imageElement = document.getElementById("dog-image")
 
 //指定のサーバーにデータを取りに行く
-fetch("https://dog.ceo/api/breeds/image/random")
+fetch("https://api.thecatapi.com/v1/images/search")
   .then((res) => {
     return res.json() //結果をjsonとして読み込んで、次のthenに渡す。
   })
